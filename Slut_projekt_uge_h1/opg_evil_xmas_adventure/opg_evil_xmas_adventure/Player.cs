aaa3ed20327013492441db85923af5ea2f21bbee
Tommy <tmth.skp@edu.mercantec.dk>
2019-12-18 10:39:49 +0100

    battle objekt okay for nu

---
+++ Slut_projekt_uge_h1/opg_evil_xmas_adventure/opg_evil_xmas_adventure/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace opg_evil_xmas_adventure
+{
+    class Player
+    {
+        public int PlayerHP;
+        public int PlayerDMG;
+        
+        Random random = new Random(); //number randomizer
+
+        public bool PlayerStats() 
+        { 
+            PlayerHP = 30;
+            PlayerDMG = random.Next(2, 6) * 2;
+            return true;
+        }
+
+       
+    }
+}
