3c03ab4fd7d10e378a1e248b053ab06c3968be29
Tommy <tmth.skp@edu.mercantec.dk>
2019-12-18 13:32:14 +0100

    text tilpasning

--- Slut_projekt_uge_h1/opg_evil_xmas_adventure/opg_evil_xmas_adventure/SwitchBoard.cs
+++ Slut_projekt_uge_h1/opg_evil_xmas_adventure/opg_evil_xmas_adventure/SwitchBoard.cs
@@ -10,24 +10,24 @@ namespace opg_evil_xmas_adventure
        {
             Obstacles obstacles = new Obstacles();
             
-            Console.WriteLine("\nAs you look down the forrest walkway. \nHidden in the middle of the thick trees you notice two paths that you can choose from");
+            Console.WriteLine("\nAs you look down the Winter forrest walkway. \nHidden in the middle of the thick trees and snow. You notice two paths that you can choose from");
             Console.WriteLine("\nDo you take the path to the right? \nOr The Path to the left? \nMaybe you wanna turnback and end your adventuring days right there?");
             Console.WriteLine("\nLeft? \nRight? \nTurnback?");
             string userResponse = Console.ReadLine();
             switch (userResponse.ToLower())
             {
                 case "left":
-                    Console.WriteLine("\nA True Hero always Turn Right first");
-                    TwoPaths();
+                    Console.WriteLine("\nA True Hero always Turn Right first \n but then again who ever said you are a Hero... \n you take the left path");
+                    obstacles.AnObstacle();
                     break;
 
                 case "right":
-                    Console.WriteLine("\nYou walk proudly for a few minutes when suddently.");
+                    Console.WriteLine("\nYou take the right path \nYou walk proudly for a few minutes when suddently.");
                     obstacles.AnObstacle();
                     break;
 
                 case "turnback":
-                    Console.WriteLine("\nYou Decide that this is enough adventuring for today. \nSo you turn back and End the game");
+                    Console.WriteLine("\nYou Decide that this is enough adventuring in this forsaken winter cold for today. \nSo you turn back and End the game");
                     Environment.Exit(0);
                     break;
                     
